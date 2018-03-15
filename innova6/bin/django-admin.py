426be7951af8ae5ba3e539125cc20bd5f12fece5
Fredy Gonzalo Captuayo Novoa <fcaptuayo@MacBook-Pro-de-Fredy.local>
2018-03-14 21:08:16 -0500

    create view of list of measurest by execute

--- innova6/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/fcaptuayo/Documents/projects/procesos/source/innova6_backend/innova6/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
