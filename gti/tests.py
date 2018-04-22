0bb9dadb7c3561f28294377831d6e85d2b28c192
captuayo <fg.captuayo@uniandes.edu.co>
2018-04-22 14:13:56 -0500

    TEST-02-create-service-by-get-all-frequent-question(ROJO)

--- gti/tests.py
+++ gti/tests.py
@@ -3,7 +3,7 @@ from django.test.client import Client
 
 from .models import Category
 from .models import Platform
-from .models import FrequentQuestions
+from .models import FrequentQuestion
 
 import json
 
@@ -30,3 +30,25 @@ class PlatformTestCase(TestCase):
         response = client.get('http://127.0.0.1:8000/platforms/')
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 2
+
+
+class FrequentQuestionsTestCase(TestCase):
+    def setUp(self):
+        category_one = Category.objects.create(category_name="Examenes")
+        category_two = Category.objects.create(category_name="Calificaciones")
+        platform_one = Platform.objects.create(platform_name="MOODLE")
+        platform_two = Platform.objects.create(platform_name="SICUA")
+        FrequentQuestion.objects.create(frequent_questions_name="SICUA", frequent_questions_category=category_one,
+                                        frequent_questions_Platform=platform_one)
+        FrequentQuestion.objects.create(frequent_questions_name="SICUA", frequent_questions_category=category_two,
+                                        frequent_questions_Platform=platform_two)
+        FrequentQuestion.objects.create(frequent_questions_name="SICUA", frequent_questions_category=category_one,
+                                        frequent_questions_Platform=platform_two)
+        FrequentQuestion.objects.create(frequent_questions_name="SICUA", frequent_questions_category=category_two,
+                                        frequent_questions_Platform=platform_one)
+
+    def test_frequent_questions_can_speak_get_all(self):
+        client = Client()
+        response = client.get('http://127.0.0.1:8000/frequent_questions/')
+        assert response.status_code == 200
+        assert len(json.loads(response.content)) == 4
