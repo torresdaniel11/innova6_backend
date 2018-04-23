40430604058e36fde3240af17f398657be179dfa
captuayo <fg.captuayo@uniandes.edu.co>
2018-04-22 21:24:10 -0500

    TEST-04-create-service-find-category-and-add-tag-plataform(VERDE)

--- gti/models.py
+++ gti/models.py
@@ -41,7 +41,8 @@ class Conversations(models.Model):
     conversation_create_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     conversation_update_date = models.DateTimeField(auto_now=True, blank=True, null=True)
     conversation_conversation_level = models.ForeignKey(ConversationLevels, editable=False, null=True, blank=True,
-                                                        on_delete=models.DO_NOTHING)
+                                                        on_delete=models.DO_NOTHING,
+                                                        default=get_conversation_levels_default)
 
     def __unicode__(self):
         return self.conversation_token
