35685e57dce62dc07d833d7ebd6ca224834f91e2
captuayo <fg.captuayo@uniandes.edu.co>
2018-04-22 20:31:45 -0500

    TEST-01-create-service-by-type-Article(ROJO)

--- gti/tests.py
+++ gti/tests.py
@@ -8,6 +8,7 @@ from .models import Conversations
 from .models import ConversationLevels
 from .models import QuestionRecords
 from .models import Questions
+from .models import TypeArticle
 
 import json
 
@@ -85,77 +86,15 @@ class RetrieveConversationsID(TestCase):
         assert response.status_code == 200
 
 
-class RetrieveRecordsConversationsID(TestCase):
+class TypeArticleTestCase(TestCase):
     def setUp(self):
-        conversation_level_qualification = ConversationLevels.objects.create(id=3,
-                                                                             conversation_level_name="Seleccionar categoria",
-                                                                             conversation_color="BLUE")
+        TypeArticle.objects.create(platform_name="INTERNO")
+        TypeArticle.objects.create(platform_name="PDF")
+        TypeArticle.objects.create(platform_name="YOUTUBE")
+        TypeArticle.objects.create(platform_name="PAGINA WEB")
 
-        category_two = Category.objects.create(category_name="Calificaciones")
-
-        question_qualification = Questions.objects.create(question_name="Pregunta de califiacion",
-                                                          question_description="calificacion",
-                                                          question_keywords="calificacion",
-                                                          question_conversation_level=conversation_level_qualification,
-                                                          question_category=category_two
-                                                          )
-
-        conversation = Conversations.objects.create(
-            id=1,
-            conversation_token='e684c238be3c8318571435637814afb394985b2625de2cff888f0fa68c23',
-            conversation_name='e684c238be3c8318571435637814afb394985b2625de2cff888f0fa68c23',
-            conversation_email="fg.captuayo@uniandes.edu.co",
-            conversation_platform="SICUA",
-            conversation_faculty="N/A",
-            conversation_conversation_level=conversation_level_qualification,
-        )
-
-        QuestionRecords.objects.create(question_record_response="Calificaciones",
-                                       question_record_conversation=conversation,
-                                       question_record_question=question_qualification,
-                                       question_record_token='e684c238be3c8318571435637814afb394985b2625de2cff888f0fa68c23',
-                                       )
-
-    def test_frequent_questions_can_speak_get_all_by_category_plataform(self):
+    def test_categories_can_speak_get_all(self):
         client = Client()
-        response = client.get(
-            'http://127.0.0.1:8000/retrieve_frequency_questions/e684c238be3c8318571435637814afb394985b2625de2cff888f0fa68c23/')
+        response = client.get('http://127.0.0.1:8000/type_article/')
         assert response.status_code == 200
-
-
-class RetrieveArticlesByTokenConversation(TestCase):
-    def setUp(self):
-        conversation_level_qualification = ConversationLevels.objects.create(id=3,
-                                                                             conversation_level_name="Seleccionar categoria",
-                                                                             conversation_color="BLUE")
-
-        category_two = Category.objects.create(category_name="Calificaciones")
-
-        question_qualification = Questions.objects.create(question_name="Pregunta de califiacion",
-                                                          question_description="calificacion",
-                                                          question_keywords="calificacion",
-                                                          question_conversation_level=conversation_level_qualification,
-                                                          question_category=category_two
-                                                          )
-
-        conversation = Conversations.objects.create(
-            id=1,
-            conversation_token='e684c238be3c8318571435637814afb394985b2625de2cff888f0fa68c23',
-            conversation_name='e684c238be3c8318571435637814afb394985b2625de2cff888f0fa68c23',
-            conversation_email="fg.captuayo@uniandes.edu.co",
-            conversation_platform="SICUA",
-            conversation_faculty="N/A",
-            conversation_conversation_level=conversation_level_qualification,
-        )
-
-        QuestionRecords.objects.create(question_record_response="Calificaciones",
-                                       question_record_conversation=conversation,
-                                       question_record_question=question_qualification,
-                                       question_record_token='e684c238be3c8318571435637814afb394985b2625de2cff888f0fa68c23',
-                                       )
-
-    def test_frequent_questions_can_speak_get_all_by_category_plataform(self):
-        client = Client()
-        response = client.get(
-            'http://127.0.0.1:8000/retrieve_frequency_questions/e684c238be3c8318571435637814afb394985b2625de2cff888f0fa68c23/')
-        assert response.status_code == 200
+        assert len(json.loads(response.content)) == 4
